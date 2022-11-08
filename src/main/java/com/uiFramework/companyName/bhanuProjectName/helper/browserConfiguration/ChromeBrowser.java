1b9eb38d954e30df8a0c8977e8eef26a629068d9
k-vamshivarun143 <78523072+k-vamshivarun143@users.noreply.github.com>
2022-11-08 22:52:17 +0530

    Add files via upload
    added new project

---
+++ src/main/java/com/uiFramework/companyName/bhanuProjectName/helper/browserConfiguration/ChromeBrowser.java
@@ -0,0 +1,67 @@
+
+package com.uiFramework.companyName.bhanuProjectName.helper.browserConfiguration;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.uiFramework.companyName.bhanuProjectName.helper.resource.ResourceHelper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * 
+ * @author Bhanu Pratap Singh
+ *
+ */
+public class ChromeBrowser {
+
+	public ChromeOptions getChromeOptions() {
+		
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--test-type");
+		option.addArguments("--disable-popup-blocking");
+		
+		DesiredCapabilities chrome = DesiredCapabilities.chrome();
+		chrome.setJavascriptEnabled(true);
+		
+		option.setCapability(ChromeOptions.CAPABILITY, chrome);	
+		//Linux
+		if(System.getProperty("os.name").contains("Linux")){
+			option.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+		}
+		return option;
+	}
+
+	public WebDriver getChromeDriver(ChromeOptions cap) {
+
+		if (System.getProperty("os.name").contains("Mac")){
+			System.out.println("we are in the -"+System.getProperty("os.name") + " platform");
+			//System.setProperty("webdriver.chrome.driver", ResourceHelper.getResourcePath("src/main/resources/drivers/chromedriver"));
+			WebDriverManager.chromedriver().setup();
+			return new ChromeDriver(cap);
+		}
+		else if(System.getProperty("os.name").contains("Window")){
+//			System.setProperty("webdriver.chrome.driver", ResourceHelper.getResourcePath("src/main/resources/drivers/chromedriver.exe"));
+			System.out.println("we are in the -"+System.getProperty("os.name") + " platform");
+			WebDriverManager.chromedriver().setup();
+			return new ChromeDriver(cap);
+		}
+		else if(System.getProperty("os.name").contains("Linux")){
+//			System.setProperty("webdriver.chrome.driver", "/usr/bin/chrome");
+			System.out.println("we are in the -"+System.getProperty("os.name") + " platform");
+			WebDriverManager.chromedriver().setup();
+			return new ChromeDriver(cap);
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		ChromeBrowser obj = new ChromeBrowser();
+		WebDriver driver = obj.getChromeDriver(obj.getChromeOptions());
+		driver.get("https://amazon.in");
+	}
+
+}
